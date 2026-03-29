21b426cf0727a6140b00459b06dd9338d21c9a3f
sonrap <rapson999@gmail.com>
2026-03-29 14:05:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -38,19 +38,3 @@ document.addEventListener('DOMContentLoaded', () => {
         observer.observe(section);
     });
 });
-
-const toggleBtn = document.getElementById('toggle-mode');
-
-toggleBtn.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-});
-
-toggleBtn.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-
-  if (document.body.classList.contains('dark-mode')) {
-    toggleBtn.textContent = 'Switch to Light Mode';
-  } else {
-    toggleBtn.textContent = 'Switch to Dark Mode';
-  }
-});
